3dcd6aab4acde0b8cbdf80448391e8d5e5c77ed5
JulienLamalle <julienlamalle63100@gmail.com>
2021-02-20 20:57:48 +0100

    added default picture and using conditionnal rendering  in case article picture are not fetch

--- components/card.jsx
+++ components/card.jsx
@@ -11,8 +11,11 @@ const Card = ({data}) => {
       {data && 
         data.articles.map(post => (
           <div className=" w-full lg:max-w-full lg:flex" key={post.url}>
-            <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${post.urlToImage})`}}>
-            </div>
+            {post.urlToImage ?
+              <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${post.urlToImage})`}}>
+              </div> : <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(/empty.svg)`, border: `0.5px #B0B0B0 solid`}}>
+              </div>
+            }
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
               <div className="mb-8">
                 <div className="text-gray-900 font-bold text-xl mb-2">{post.title}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {},
   },

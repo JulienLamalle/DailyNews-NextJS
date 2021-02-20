ed12a6ca0547d37ce5ace70c823709d9c3b677e1
JulienLamalle <julienlamalle63100@gmail.com>
2021-02-20 20:59:35 +0100

    added default picture and using conditionnal rendering  in case article picture are not fetch

--- components/card.jsx
+++ components/card.jsx
@@ -1,43 +1,55 @@
 import React from "react";
-import dayjs from 'dayjs';
-import fr from 'dayjs/locale/fr'
-import Link from 'next/link';
+import dayjs from "dayjs";
+import fr from "dayjs/locale/fr";
+import Link from "next/link";
 
-const Card = ({data}) => {
-  dayjs.locale('fr')
+const Card = ({ data }) => {
+  dayjs.locale("fr");
 
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-5">
-      {data && 
-        data.articles.map(post => (
+      {data &&
+        data.articles.map((post) => (
           <div className=" w-full lg:max-w-full lg:flex" key={post.url}>
-            {post.urlToImage ?
-              <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${post.urlToImage})`}}>
-              </div> : <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(/empty.svg)`, border: `0.5px #B0B0B0 solid`}}>
-              </div>
-            }
+            {post.urlToImage ? (
+              <div
+                className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                style={{ backgroundImage: `url(${post.urlToImage})` }}
+              ></div>
+            ) : (
+              <div
+                className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                style={{
+                  backgroundImage: `url(/empty.svg)`,
+                  border: `0.5px #B0B0B0 solid`,
+                }}
+              ></div>
+            )}
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
               <div className="mb-8">
-                <div className="text-gray-900 font-bold text-xl mb-2">{post.title}</div>
+                <div className="text-gray-900 font-bold text-xl mb-2">
+                  {post.title}
+                </div>
                 <p className="text-gray-700 text-base">{post.description}</p>
               </div>
               <div className="flex items-center">
-                <Link href={post.url}>  
+                <Link href={post.url}>
                   <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mr-3 rounded-full">
                     Voir l'article
                   </button>
                 </Link>
                 <div className="text-sm">
                   <p className="text-gray-900 leading-none">{post.author}</p>
-                  <p className="text-gray-600">{dayjs(`${post.publishedAt}`).format('DD MMMM YYYY')}</p>
+                  <p className="text-gray-600">
+                    {dayjs(`${post.publishedAt}`).format("DD MMMM YYYY")}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        ))
-      }
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default Card;

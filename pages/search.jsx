6e42e8c163ebafa49942df3949d58f8ebf0c8d42
JulienLamalle <julienlamalle63100@gmail.com>
2021-02-21 05:51:18 +0100

    added no-cors mode on search page

--- pages/search.jsx
+++ pages/search.jsx
@@ -35,6 +35,7 @@ export default function Search() {
     const proxyUrl = 'https://thingproxy.freeboard.io/fetch/';
     try {
       await fetch(`${proxyUrl}https://api.aylien.com/news/stories?body=${checker(query)}&language=fr&per_page=21`, {
+      mode: 'no-cors',
       method: 'get',
       headers: {
         'X-AYLIEN-NewsAPI-Application-ID' : `${process.env.NEXT_PUBLIC_NEWS_APPID}`,
